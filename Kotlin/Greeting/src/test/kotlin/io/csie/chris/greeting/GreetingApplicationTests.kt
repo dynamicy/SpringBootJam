9976f4a0da0002c428bfd60887c831cce5958669
chris <chris@csie.io>
2024-04-23 19:07:18 +0800

    Implementation of health check.

---
+++ Kotlin/Greeting/src/test/kotlin/io/csie/chris/greeting/GreetingApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.csie.chris.greeting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
